6c30806681dfac9101a1115ed59f644616f73314
Mark <73597484+Ivanov9090@users.noreply.github.com>
2024-06-03 16:03:21 +0300

    Update Main.java

--- src/main/java/ru/ivanov9090/Main.java
+++ src/main/java/ru/ivanov9090/Main.java
@@ -5,6 +5,6 @@ import ru.ivanov9090.QuestionLogic.Question;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
-        new TelegramBot("TelegramBot", "7137375302:AAG6pPU3ARjVtHVgCEfruRwDAmc9-8xkUOM");
+        new TelegramBot("TelegramBot", "");
     }
 }
